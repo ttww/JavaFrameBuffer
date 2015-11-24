9cddb64fa3b0f1289995afe3c8c98b1fe880a9ee
robin <robin.kirkman@gmail.com>
2015-11-23 23:36:20 -0500

    Simple test that tries to load the native lib

---
+++ JavaFrameBuffer/src/test/java/org/tw/pi/framebuffer/FrameBufferTest.java
@@ -0,0 +1,10 @@
+package org.tw.pi.framebuffer;
+
+import org.junit.Test;
+
+public class FrameBufferTest {
+	@Test
+	public void testLoad() throws Exception {
+		Class.forName(FrameBuffer.class.getName());
+	}
+}
