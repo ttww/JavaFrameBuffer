8348a29fa6c55ead4315ff84fd1da6c77f2bbecc
robin <robin.kirkman@gmail.com>
2015-11-29 17:05:42 -0500

    clocky test

--- framebuffer/src/test/java/org/tw/pi/framebuffer/FrameBufferTest.java
+++ framebuffer/src/test/java/org/tw/pi/framebuffer/FrameBufferTest.java
@@ -1,7 +1,12 @@
 package org.tw.pi.framebuffer;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.Calendar;
+
 import org.junit.Test;
 
 public class FrameBufferTest {
@@ -10,16 +15,43 @@ public class FrameBufferTest {
 		Class.forName(FrameBuffers.class.getName());
 	}
 	
+	private static Point project(Point center, int value, int max, int length) {
+		double radians = (value / (double) max) * (2 * Math.PI);
+		Point p = new Point(center.x, center.y);
+		p.x += length * Math.cos(radians);
+		p.y += length * Math.sin(radians);
+		return p;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		FrameBufferedImage fb = new FrameBufferedImage(args[0]);
 		try {
-			Graphics g = fb.getGraphics();
-			int m = 0xff;
-			for(int i = 0; i < Math.min(fb.getWidth(), fb.getHeight()) / 2; i++) {
-				g.setColor(new Color(m));
-				g.drawRect(i, i, fb.getWidth() - 2*i, fb.getHeight() - 2*i);
-				m <<= 1;
-				m |= (m >>> 24);
+			Graphics2D g = (Graphics2D) fb.getGraphics();
+			Dimension dim = new Dimension(fb.getWidth(), fb.getHeight());
+			Point center = new Point(dim.width / 2, dim.height / 2);
+			int radius = Math.min(dim.width, dim.height) / 2;
+			Calendar c = Calendar.getInstance();
+			while(true) {
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 0, dim.width, dim.height);
+				g.setColor(Color.WHITE);
+				g.drawOval(center.x - radius, center.y - radius, radius * 2, radius * 2);
+
+				c.setTimeInMillis(System.currentTimeMillis());
+				int hour = c.get(Calendar.HOUR);
+				int minute = c.get(Calendar.MINUTE);
+				int second = c.get(Calendar.SECOND);
+				
+				Point p = project(center, hour, 12, radius * 2 / 3);
+				g.drawLine(center.x, center.y, p.x, p.y);
+				
+				p = project(center, minute, 60, radius * 3 / 4);
+				g.drawLine(center.x, center.y, p.x, p.y);
+				
+				p = project(center, second, 60, radius * 5 / 6);
+				g.drawLine(center.x, center.y, p.x, p.y);
+				
+				Thread.sleep(500);
 			}
 		} finally {
 			fb.close();
