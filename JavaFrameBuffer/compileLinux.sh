4190d5da49938beb4b04e09c2366f67283fe8632
chris <acomputerdog@gmail.com>
2017-06-15 17:52:37 -0400

    Fix linking errors under x86_64

--- JavaFrameBuffer/compileLinux.sh
+++ JavaFrameBuffer/compileLinux.sh
@@ -8,13 +8,13 @@ jniSysHeader=/opt/jdk1.7.0_45/include/linux
 rm "$jniResult" 2>/dev/null
 
 echo "javah"
-javah -d src/main/c  -classpath bin org.tw.pi.framebuffer.FrameBuffer
+javah -d src/main/c -classpath bin org.tw.pi.framebuffer.FrameBuffer
 rm src/main/c/org_tw_pi_framebuffer_FrameBuffer_ScreenPanel.h
 rm src/main/c/org_tw_pi_framebuffer_FrameBuffer_UpdateThread.h
 
 
 echo "gcc"
-gcc -Wall -O2 -o "$jniResult" -shared -I "$jniJdkHeader"  -I "$jniSysHeader" -I src/main/c src/main/c/FrameBuffer.c 
+gcc -Wall -O2 -fPIC -o "$jniResult" -shared -I "$jniJdkHeader"  -I "$jniSysHeader" -I src/main/c src/main/c/FrameBuffer.c
 
 echo
 ls -l "$jniResult"
